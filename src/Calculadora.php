f837c41abc901002caea6f77c3561a3119e0bd6f
xrengor <rengorssj@gmail.com>
2026-06-03 15:45:08 -0500

    chore: archivos base del proyecto con estructura inicial

---
+++ src/Calculadora.php
@@ -0,0 +1,20 @@
+<?php
+namespace App;
+
+class Calculadora {
+    public function sumar($a, $b) {
+        return $a + $b;
+    }
+
+    public function restar($a, $b) {
+        return $a - $b;
+    }
+
+    public function multiplicar($a, $b) {
+        return $a * $b;
+    }
+
+    public function dividir($a, $b) {
+        return 0; 
+    }
+}
