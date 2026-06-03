d08d27584ebbe510ccd058101cbd6432cf87ea54
xrengor <rengorssj@gmail.com>
2026-06-03 15:53:23 -0500

    feat: manejada division por cero con excepcion (TDD paso 4)

--- src/Calculadora.php
+++ src/Calculadora.php
@@ -15,6 +15,9 @@ class Calculadora {
     }
 
     public function dividir($a, $b) {
+        if ($b == 0) {
+            throw new \InvalidArgumentException("No se puede dividir por cero.");
+        }
         return $a / $b;
     }
 }
